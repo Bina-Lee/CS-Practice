be7d9bd53fe6b5d430fb797aecd88436a5de3160
Herb <Herb@DESKTOP-C583OK2>
2022-08-06 03:32:03 +0900

    asdf

---
+++ CS Practice/20220802/20220802.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//static
+
+namespace CS_Practice._20220802
+{
+    public static class _20220802
+    {
+        //정적멤버변수
+        //객체에 귀속된게 아니라
+        //클래스 그 자체에 귀속됨
+        //데이터영역에 메모리가 올라감
+        static int countasdf;
+    }
+}
+
+//this 레퍼런스
+
+namespace chap15
+{
+    class rmfjrpdy
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
